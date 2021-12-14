3fd6ebc360fdb67f327d5f3c3e464870cfa75841
Elgar <45301805+Elgar17@users.noreply.github.com>
2021-12-14 10:28:45 +0800

    添加搜索框

--- docs/.vuepress/components/InputSearch.vue
+++ docs/.vuepress/components/InputSearch.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="input-search-wrap">
-    <!-- <select v-model="ingin">
-      <option value="baidu">百度</option>
-      <option value="google">谷歌</option>
-      <option value="bing">必应</option>
-    </select> -->
     <div class="logo-wrapper">
-      <img src="../public/kz321.top.png" alt="">
+      <img src="../public/kz321.top.png" alt="" />
     </div>
     <div class="input-search">
-      <ul>
-        <li>百度</li>
-        <li>谷歌</li>
-        <li>必应</li>
+      <ul class="ingin-wrapper">
+        <li
+          v-for="(item, index) in ingins"
+          :key="index"
+          @click="current = item"
+          :class="{ slected: current == item }"
+        >
+          {{ item }}
+        </li>
       </ul>
       <input type="text" v-model="input" placeholder="请输入" />
       <button @click="search">搜索</button>
@@ -24,102 +24,131 @@
 <script>
 export default {
   name: "InputSearch",
-  data () {
+  data() {
     return {
-      // ingin: "baidu",
+      current: "百度",
+      ingins: ["百度", "谷歌", "必应", "翻译"],
       input: "",
       baidu: "https://www.baidu.com/s?ie=UTF-8&wd=",
       google: "https://www.google.com/search?ie=utf-8&q=",
-      bing: "https://cn.bing.com/search?q="
-    }
+      bing: "https://cn.bing.com/search?q=",
+      fanyi: "https://fanyi.baidu.com/translate#zh/en/"
+    };
   },
   methods: {
-    // search(){
-    //   if(this.input.trim() == "") return;
-    //   switch(this.ingin){
-    //     case "baidu":
-    //       window.open(this.baidu + this.input)
-    //       break;
-    //     case "google":
-    //       window.open(this.google + this.input)
-    //       break;
-    //     case "bing":
-    //       window.open(this.bing + this.input)
-    //       break;
-    //   }
-    // }
-  }
+    search() {
+      if (this.input.trim() == "") return;
+      switch (this.current) {
+        case "百度":
+          window.open(this.baidu + this.input);
+          break;
+        case "谷歌":
+          window.open(this.google + this.input);
+          break;
+        case "必应":
+          window.open(this.bing + this.input);
+          break;
+        case "翻译":
+          window.open(this.fanyi + this.input);
+          break;
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-@media screen and (max-width: 600px) {
+/* 小于780px */
+@media screen and (max-width: 780px) {
   .ctrld {
     display: none;
   }
+  .input-search {
+    margin-left: 0;
+  }
+  .input-search,
+  .logo-wrapper {
+    display: block;
+    text-align: center;
+  }
+  .logo-wrapper {
+    float: none;
+    height: 80px;
+    margin: 0 auto;
+  }
 }
-
+/* 大于 780px */
+@media screen and (min-width: 780px) {
+  .logo-wrapper {
+    float: left;
+  }
+  .input-search {
+    display: block;
+    padding-left: 60px;
+    margin-left: 200px;
+  }
+}
 .input-search-wrap {
-  padding-top: 20px;
+  padding: 20px 0;
   font-size: 12px;
-  height: 80px;
-  text-align: center;
   direction: ltr;
-  /* background-color: #143bd0; */
 }
-.logo-wrapper{
-  float: left;
+.logo-wrapper {
   height: 80px;
   width: 200px;
   text-align: center;
 }
-.input-search{
-  margin-left: 200px;
+.logo-wrapper img {
+  border-radius: 8px;
 }
 
-select,
+.ingin-wrapper {
+  margin: 0;
+}
+.ingin-wrapper li {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  border-radius: 5px 5px 0 0;
+  list-style: none;
+  padding: 0 12px;
+  margin: 0 12px;
+  cursor: pointer;
+}
+
+.slected {
+  color: #fff;
+  background: #143bd0;
+}
+
 input,
 button {
   height: 35px;
   line-height: 35px;
   outline: none;
   vertical-align: middle;
-  border: 1px solid #ccc;
+  border: 1px solid #143bd0;
   font-size: 16px;
   border-radius: 4px 0 0 4px;
 }
-
-select {
-  border-radius: 4px 0 0 4px;
-  box-sizing: border-box;
-  vertical-align: middle;
-  appearance: none; /*清除select下拉框默认样式*/
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  padding: 0 16px 0 14px; /*为下拉小箭头留出一点位置，避免被文字覆盖*/
-  /* font-weight: bold; */
-  /* 自定义图片覆盖原有的下三角符号 */
-  background: url('./xiala.png') no-repeat scroll right center transparent;
-}
-select::-ms-expand {
-  display: none; /*清除IE默认下拉按钮，但是测试发现IE10以上有效，IE8，9默认下拉按钮仍旧存在*/
-}
-.input-search-wrap input {
-  width: 300px;
+.input-search-wrap .input-search input {
+  width: 60%;
   box-sizing: border-box;
   vertical-align: middle;
   padding: 0 14px;
   outline: none;
   margin-left: 1px;
 }
-.input-search-wrap button {
+.input-search-wrap .input-search button {
   padding: 0 14px;
   margin: -1px;
   border-radius: 0 4px 4px 0;
   font-weight: bold;
   background-color: #fff;
+  color: #fff;
+  background: #143bd0;
 }
-.input-search-wrap button:hover {
+.input-search-wrap .input-search button:hover {
   cursor: pointer;
 }
 </style>
